f0400df589f1960f5b077a3a0c51a7c29c98eb46
NevDev48 <nevriskayosia@gmail.com>
2025-08-06 15:19:47 +0800

    add Hero component with introductory content and statistics

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import Navbar from './components/navbar'
+import Hero from './components/Hero'
 
 function App() {
   return (
     <div className="min-h-screen">
       <Navbar />
-      
+      <Hero />
     </div>
   )
 }
